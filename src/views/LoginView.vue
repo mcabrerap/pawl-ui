54723ce4c04c5cc1491ea0a99ea1ac534b6261f0
Cristian Giraldo Zúñiga <cristian.zuniga@rappi.com>
2023-01-15 11:38:54 -0500

    added signup page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-head">
+      <div class="header">
+        <h1 class="header__title">Pawl <span>UI</span></h1>
+      </div>
+    </div>
+    <div class="hero-body">
+      <div class="login">
+        <div class="login__header">
+          <h2 class="login__title">Welcome back</h2>
+          <p class="has-text-grey-light">Please login to your account</p>
+        </div>
+        <div class="field mb-6">
+          <label class="label is-large">Username</label>
+          <div class="control">
+            <input class="input is-large" type="text" placeholder="Text input">
+          </div>
+        </div>
+        <div class="field mb-6">
+          <label class="label is-large">Password</label>
+          <div class="control">
+            <input class="input is-large" type="text" placeholder="Text input">
+          </div>
+        </div>
+        <div class="field mb-6">
+          <div class="control">
+            <button class="button is-primary is-large is-fullwidth is-rounded">Login</button>
+          </div>
+        </div>
+        <div class="login__signup">
+          <p>Don't have an account? <span>Sign up</span></p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
