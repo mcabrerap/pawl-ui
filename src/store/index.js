99c6802f03f3f0da20f30c22d038160548c92dcd
Cristian Giraldo Zúñiga <cristian.zuniga@rappi.com>
2023-01-21 16:55:10 -0500

    added vuex, axios and validations

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,41 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import http from '../http/axios.http.js'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        signUpUser: {
+            fullName: null,
+            email: null,
+        }
+    },
+    getters: {
+        getSignUpUser: state => state.signUpUser
+    },
+    mutations: {
+
+        setSignUpUserData(state, payload) {
+            state.signUpUser = payload
+        }
+
+    },
+    actions: {
+        async signUp(context, payload) {
+
+            console.log('signUp action', payload)
+            const signUpResponse = await http.post('/pawl/v1/api/signup', {
+                full_name: payload.fullName,
+                email: payload.email,
+                password: payload.password
+            })
+
+            if (signUpResponse.status === 201) {
+
+                context.commit('setSignUpUserData', {
+                    fullName: payload.fullName,
+                    email: payload.email
+                })
+            }
+        }
+    },
+    modules: {}
 })
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -6,7 +6,7 @@
       </div>
     </div>
     <div class="hero-body">
-      <div class="signup">
+      <form class="signup" @submit.prevent="submitForm">
         <div class="signup__header">
           <h2 class="signup__title">Create your account</h2>
           <p class="has-text-grey-light">Enter the fields below to get started</p>
@@ -14,20 +14,38 @@
         <div class="field mb-6">
           <label class="label is-large">Full name*</label>
           <div class="control">
-            <input class="input is-large" type="text" placeholder="Full name">
+            <input
+                class="input is-large"
+                type="text"
+                placeholder="Full name"
+                maxlength="50"
+                v-model.trim="fullName">
           </div>
         </div>
         <div class="field mb-6">
           <label class="label is-large">Email address*</label>
           <div class="control">
-            <input class="input is-large" type="email" placeholder="Email">
+            <input
+                class="input is-large"
+                type="email"
+                placeholder="Email"
+                maxlength="50"
+                v-model.trim="email">
           </div>
         </div>
         <div class="field mb-6">
           <label class="label is-large">Password*</label>
           <div class="control">
-            <input class="input is-large" type="password" placeholder="Password">
+            <input
+                class="input is-large"
+                type="password"
+                placeholder="Password"
+                maxlength="15"
+                v-model.trim="password">
           </div>
+        </div>
+        <div class="signup__validation-error" v-if="formIsValid">
+          <p v-for="error in submitFormErrors" :key="error">{{ error }}</p>
         </div>
         <div class="field mb-6">
           <div class="control">
@@ -37,7 +55,51 @@
         <div class="signup__signup">
           <p>Already have an account? <span><router-link to="/login">Log in</router-link></span></p>
         </div>
-      </div>
+        <div v-if="test">
+          ENTRE!!!!!!!!!!
+        </div>
+      </form>
     </div>
   </section>
 </template>
+
+<script setup>
+import {ref, computed} from 'vue'
+import {useStore} from 'vuex'
+
+
+const store = useStore()
+
+const fullName = ref('')
+const email = ref('')
+const password = ref('')
+const submitFormErrors = ref([])
+
+const test = computed(() => {
+  return store.state.signUpUser.email
+})
+
+const submitForm = () => {
+
+  if (formIsValid()) {
+    store.dispatch('signUp', {fullName: fullName.value, email: email.value, password: password.value})
+  }
+}
+
+const formIsValid = () => {
+
+  submitFormErrors.value = []
+
+  if (fullName.value.length < 3) {
+    submitFormErrors.value.push('Full name must be at least 3 characters long')
+  }
+  if (email.value.length < 5) {
+    submitFormErrors.value.push('Email must be at least 5 characters long')
+  }
+  if (password.value.length < 8) {
+    submitFormErrors.value.push('Password must be at least 8 characters long')
+  }
+
+  return submitFormErrors.value.length === 0
+}
+</script>
